2bc50167772d18b80396448df5ef50ee6e8e743f
toprakunal <toprakunal@hotmail.com>
2023-07-15 09:31:09 +0300

    This is the first commit.

---
+++ src/main/java/com/example/equipmentmanagement/EquipmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.equipmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/equipmentmanagement/EquipmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
